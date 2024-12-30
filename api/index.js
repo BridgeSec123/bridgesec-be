9387007565c4f37016cb44c38f5b1c60de969b21
Aparana singh <aparana.singh@adjecti.com>
2024-12-30 11:49:49 +0530

    update location of index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // server.js
 require('dotenv').config();
-const { app } = require('./app');
-const logger = require('./utils/logger');
+const { app } = require('../app');
+const logger = require('../utils/logger');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
